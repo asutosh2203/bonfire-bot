448deb3c41bd78743eff28362db62546e9cf857f
asutosh2203 <asutosh2001@gmail.com>
2026-01-01 04:13:24 +0530

    fix(bot): change tools type to any

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -43,7 +43,7 @@ export async function POST(req: Request) {
       `;
     }
 
-    const tools: Tool[] = [
+    const tools: any = [
       {
         googleSearch: {}, // 👈 This is all you need. It activates the grounding.
       },
